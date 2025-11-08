d1c3e39d8b10f473d4441231e14ca5f1f29d604b
Mat <vidalmat06@gmail.com>
2025-11-08 10:54:06 +0100

    feat: add new migration for users table to replace name with firstname and lastname + seeder modification

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,8 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'lastname',
         'email',
         'password',
     ];
@@ -44,5 +45,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function getFullNameAttribute(): string
+    {
+        return trim(($this->firstname ?? '') . ' ' . ($this->lastname ?? ''));
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
+            'firstname' => 'John',
+            'lastname' => 'Doe',
             'email' => 'test@example.com',
         ]);
+
+        User::factory()->count(15)->create();
     }
 }
